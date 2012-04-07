fb88c23d2247e815ce3fb8aef9d963560b5de94f
Francisco Dibar <frandibar@gmail.com>
2012-04-07 13:52:03 -0300

    agregue licencia

--- canasta/model.py
+++ canasta/model.py
@@ -1,20 +1,44 @@
 # -*- coding: latin-1 -*-
 
+#-------------------------------------------------------------------------------
+# Copyright (C) 2012 Francisco Dibar
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#-------------------------------------------------------------------------------
+
 from camelot.model import metadata
 
 __metadata__ = metadata
 
 from camelot.admin.action import Action
-from camelot.view.action_steps import FlushSession
+from camelot.view.action_steps import FlushSession, MessageBox
 from camelot.admin.entity_admin import EntityAdmin
 from camelot.view.filters import ComboBoxFilter
 from elixir import Entity, Field, ManyToOne, OneToMany, using_options
-from sqlalchemy import Unicode, Date, Float, Integer, String, Boolean
+from sqlalchemy import Unicode, Date, Float, Integer, Boolean
+from sqlalchemy.exc import IntegrityError, FlushError
+from PyQt4.QtGui import QMessageBox
 import camelot
 import datetime
 
 class Supermercado(Entity):
-    using_options(tablename="supermercado")
+    using_options(tablename="supermercado", order_by=["denominacion"])
     denominacion = Field(Unicode(50), required=True)
     domicilio = Field(Unicode(100), required=True)
 
@@ -25,7 +49,7 @@ class Supermercado(Entity):
         list_display = ["denominacion", "domicilio"]
 
 class Categoria(Entity):
-    using_options(tablename="categoria")
+    using_options(tablename="categoria", order_by=["descripcion"])
     descripcion = Field(Unicode(50), required=True)
 
     def __unicode__(self):
@@ -35,15 +59,15 @@ class Categoria(Entity):
         list_display = ["descripcion"]
 
 class Articulo(Entity):
-    using_options(tablename="articulo")
+    using_options(tablename="articulo", order_by=["descripcion"])
     categoria = ManyToOne("Categoria", required=True)
     descripcion = Field(Unicode(100), required=True)
     marca = ManyToOne("Marca", required=True)
     cantidad = Field(Float, default=1, required=True)
-    unidad_medida = Field(String(25))
-    envase = Field(String(25))
+    unidad_medida = Field(Unicode(25))
+    envase = Field(Unicode(25))
     foto = Field(camelot.types.Image(upload_to="fotos"))
-    codigo_barras = Field(Unicode(25))
+    codigo_barras = Field(Unicode(25))  # unicode en vez de string porque sino no permite filtrado
 
     def __unicode__(self):
         envase = self.envase if self.envase else ""
@@ -60,12 +84,20 @@ class Articulo(Entity):
                         "unidad_medida",
                         "categoria",
                         "envase",
-                        "foto",
                         "codigo_barras",
+                        ]
+        form_display = ["descripcion",
+                        "marca",
+                        "cantidad",
+                        "unidad_medida",
+                        "categoria",
+                        "envase",
+                        "codigo_barras",
+                        "foto",
                         ]
 
 class Marca(Entity):
-    using_options(tablename="marca")
+    using_options(tablename="marca", order_by=["denominacion"])
     denominacion = Field(Unicode(30), unique=True)
 
     def __unicode__(self):
@@ -77,7 +109,7 @@ class Marca(Entity):
 class Precio(Entity):
     using_options(tablename="precio")
     articulo = ManyToOne("Articulo", primary_key=True)
-    fecha = Field(Date, primary_key=True)
+    fecha = Field(Date, primary_key=True, default=datetime.date.today)
     supermercado = ManyToOne("Supermercado", primary_key=True)
     precio = Field(Float)
 
@@ -124,19 +156,53 @@ class ImputarCarrito(Action):
     def model_run(self, model_context):
         # agregar estos campos a la tabla de pagos, solo si no existen.
         obj = model_context.get_object()
+        error_occurred = False
         if obj.imputado:
-            # TODO show messagebox
-            print "Ya fue imputado!"
+            yield MessageBox("El carrito ya ha sido imputado.",
+                             icon=QMessageBox.Warning,
+                             title="Imputar carrito",
+                             standard_buttons=QMessageBox.Ok)
             return
         for art in obj.articulos:
-            row = Precio()
-            row.articulo = art.articulo
-            row.precio = art.precio
-            row.fecha = obj.fecha
-            row.supermercado = obj.supermercado
-            Precio.query.session.flush()
-        obj.imputado = True
+            try:
+                row = Precio()
+                row.articulo = art.articulo
+                row.precio = art.precio
+                row.fecha = obj.fecha
+                row.supermercado = obj.supermercado
+                Precio.query.session.flush()
+            except FlushError, e:
+                error_occurred = True
+                yield MessageBox("Se ha producido un error de flush:\n\n%s" % e,
+                                 icon=QMessageBox.Critical,
+                                 title="Imputar carrito",
+                                 standard_buttons=QMessageBox.Ok)
+                return
+            except IntegrityError, e:
+                error_occurred = True
+                yield MessageBox(u"Se ha producido un error, quiza el siguiente artículo se encuentra repetido:\n\n%s" % e,
+                                 icon=QMessageBox.Critical,
+                                 title="Imputar carrito",
+                                 standard_buttons=QMessageBox.Ok)
+                return
+            except Exception, e:
+                error_occurred = True
+                yield MessageBox("Se ha producido un error:\n\n%s" % e,
+                                 icon=QMessageBox.Critical,
+                                 title="Imputar carrito",
+                                 standard_buttons=QMessageBox.Ok)
+                return
+        obj.imputado = not error_occurred
         yield FlushSession(model_context.session)
+        # try:
+        #     yield FlushSession(model_context.session)
+        # except Exeption, e:
+        #         yield MessageBox("Se ha producido un error:\n\n%s" % e,
+        #                          icon=QMessageBox.Critical,
+        #                          title="Imputar carrito",
+        #                          standard_buttons=QMessageBox.Ok)
+        #         return
+
 
 class Carrito(Entity):
     using_options(tablename="carrito")
@@ -155,5 +221,5 @@ class Carrito(Entity):
                         ]
         list_display = ["supermercado", "fecha", "imputado"]
         form_actions = [ImputarCarrito()]
-        field_attributes = dict(imputado = dict(editable = False))
+        field_attributes = dict(imputado = dict(editable = True)) # TODO cambiar al false en produccion
         form_size = (800,600)
--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+#-------------------------------------------------------------------------------
+# Copyright (C) 2012 Francisco Dibar
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#-------------------------------------------------------------------------------
 
 
 #
@@ -18,12 +39,19 @@ from setuptools import setup, find_packages
 logging.basicConfig( level=logging.INFO )
 
 setup(
-    name = 'My Application',
+    name = 'Canasta Familiar',
     version = '1.0',
-    author = 'My Company',
+    author = 'Francisco Dibar',
+    author_email = 'frandibar+canasta@gmail.com',
+    maintainer = 'Francisco Dibar',
+    maintainer_email = 'frandibar+canasta@gmail.com',
     url = 'http://www.python-camelot.com',
+    license = 'MIT',
     include_package_data = True,
+    install_requires = ['Camelot>=11.12.30',
+                        'MySQL-python>=1.2.1'],
     packages = find_packages(),
+    platforms = 'Linux',
     py_modules = ['settings', 'main'],
     entry_points = {'gui_scripts':[
                      'main = main:start_application',
@@ -37,8 +65,8 @@ setup(
                        'timestamp':datetime.datetime.now(),
                        },
         'wininst_cloud':{ 'excludes':'excludes.txt'},
-    }, 
+    },
 
   )
 
-    
+
